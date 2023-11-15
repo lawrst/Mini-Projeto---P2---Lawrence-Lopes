2f3bc7d903f51be709781f4dd402d95deb84b993
lawrst <lawrencelopesgomes@gmail.com>
2023-11-14 22:50:09 -0300

    mini projeto 4

--- App.java
+++ App.java
@@ -4,43 +4,34 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Personagem principal = new Personagem("Lawrence", 100, 100);
-
-        Personagem fazendeiro = new Personagem("Link", 100, 200);
-
+        Personagem fazendeiro = new Personagem("Link", 100, 100);
         Personagem monstro = new Personagem("Lobo", 200, 100);
 
         Scanner ler = new Scanner(System.in);
 
-        Capitulo capitulo1 = new Capitulo("1", principal, ler);       
-        capitulo1.Texto = "Você acaba de cair em um mundo desconhecido onde está tudo nublado, exceto a única coisa que você consegue ver:"+
-        " uma árvore amarela com folhas caindo. Você escuta o som de um animal, porém a cada segundo, o barulho só aumenta.";
-        capitulo1.Escolha_1 = "Ficar esperando para ver o animal.";
-        capitulo1.Escolha_2 = "Se levantar e sair correndo.";
+        Capitulo capitulo1 = new Capitulo("Capitulo 1: ", "Você acaba de cair em um mundo desconhecido onde está tudo nublado, exceto a única coisa que você consegue ver:"+
+        " uma árvore amarela com folhas caindo. Você escuta o som de um animal, porém a cada segundo, o barulho só aumenta.", principal, ler,
+        new String[]{"Ficar esperando para ver o animal.", "Se levantar e sair correndo."});
+    
 
-
-        Capitulo capitulo2 = new Capitulo("2", principal, ler);
-        
-        capitulo2.Texto = "[FINAL 1] - Você aguardou até ver que era um lobo-branco, famoso por assassinar viajantes.\n" + "Você corre e encontra uma madeira, conseguindo atingir o lobo que"+
+        Capitulo capitulo2 = new Capitulo("[FINAL 1]", "- Você aguardou até ver que era um lobo-branco, famoso por assassinar viajantes.\n" + "Você corre e encontra uma madeira, conseguindo atingir o lobo que"+
         " recua, deixando escapar umas gotas de sangue pelo chão.\nInfelizmente você não teve chance." +
-        "\nVocê morreu sendo devorado.";
+        "\nVocê morreu sendo devorado.", principal, ler,
+        new String[]{});
 
-        Capitulo capitulo3 = new Capitulo("3", principal, ler);
-        
-        capitulo3.Texto = "Você sai correndo e após um tempo, o animal para de correr em sua direção. Você vê uma humilde casa de taipa e sem teto, lhe vêm a mente se há algum morador.";
+        Capitulo capitulo3 = new Capitulo("Capitulo 3", "Você sai correndo e após um tempo, o animal para de correr em sua direção. Você vê uma humilde casa de taipa e sem teto, lhe vêm a mente se há algum morador.",
+        principal, ler,
+        new String[]{"Entrar na casa para investigar.", "Sair andando na esperanca de algo melhor."});
 
-        capitulo3.Escolha_1 = "Entrar na casa para investigar.";
-
-        capitulo3.Escolha_2 = "Sair andando na esperança de algo melhor.";
-
-        Capitulo capitulo4 = new Capitulo("4", principal, ler);
-        capitulo4.Texto = "[FINAL 2] \nVocê decide entrar na humilde casa, porém um fazendeiro lhe confunde com um ladrão pelas suas vestes.\n" +
+        Capitulo capitulo4 = new Capitulo("[FINAL 2]", "\nVocê decide entrar na humilde casa, porém um fazendeiro lhe confunde com um ladrão pelas suas vestes.\n" +
         "Olá, meu nome é " + fazendeiro.nome + ", e quem ousa entrar em minha casa?\n" +
         "Ele lhe ameaça com uma enxada, você tenta correr, mas não consegue escapar." +
-        "\nVocê morreu com a cabeça decepada.";
+        "\nVocê morreu com a cabeça decepada.", principal, ler,
+        new String[]{});
 
-        Capitulo capitulo5 = new Capitulo("5", principal, ler);
-        capitulo5.Texto = "Você decide sair dalí.\nApós andar, andar e andar, você não encontra nada e está com fome e frio.\n" +
-        "Sua energia está quase esgotada e você decide sentar no chão. Você faleceu por hipotermia.";
+        Capitulo capitulo5 = new Capitulo("Capitulo 5", "Você decide sair dalí.\nApós andar, andar e andar, você não encontra nada e está com fome e frio.\n" +
+        "Sua energia está quase esgotada e você decide sentar no chão. Você faleceu por hipotermia.", principal, ler,
+        new String[]{});
 
         capitulo1.mostrar();
 
--- Capitulo.java
+++ Capitulo.java
@@ -4,33 +4,51 @@ import java.util.Scanner;
 public class Capitulo {
     String Nome;
     String Texto;
-    String Escolha_1;
-    String Escolha_2;
+    String[] Escolhas;
     Personagem personagem;
     int alteracaoEnergia;
     Scanner escaneador;
 
-    public Capitulo(String nome, Personagem personagem, Scanner escaneador) {
-        Nome = nome;
+    public Capitulo(String nome, String Texto, Personagem personagem, Scanner escaneador, String[] Escolhas) {
+        this.Nome = nome;
         this.personagem = personagem;
         this.escaneador = escaneador;
+        this.Escolhas = Escolhas;
+        this.Texto = Texto;
+        
     }
 
     public void mostrar(){
-        System.out.println("Capitulo: "+ Nome);
         System.out.println(Texto);
+
+        for(int i = 0; i < Escolhas.length; i++){
+            System.out.println("- " + Escolhas[i]);
+        }
     }
 
     public int escolher(){
-        System.out.println("1 - "+Escolha_1);
-        System.out.println("2 - "+Escolha_2);
-        String lendo = escaneador.nextLine();
-        if (lendo.equals("1")){
-            return 1;
-        } else {
-            return 2;
-        }
+        int Escolhido = 0;
+        String escolha;
+        boolean valido = false;
 
+        if(Escolhas.length == 0){
+            System.out.println("Fim de jogo! Reinicie para começar novamente!");
+            return -1;
+        } else {
+            while (!valido){
+                escolha = escaneador.nextLine();
+
+                for(int i = 0; i < Escolhas.length; i++){
+                    if(escolha.equals(Escolhas[i])){
+                        valido = true;
+                        Escolhido = i + 1;
+                    }
+                }
+                if(!valido){
+                    System.out.println("Digite novamente!");
+                }
+            }
+            return Escolhido;
+        }
+       }
     }
-    
-}
